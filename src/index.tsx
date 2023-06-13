979a36ae92e1d0aeaa88e0e0632a9ea8a7181405
Maxim Egorov <13maximegorov@gmail.com>
2023-06-13 23:35:56 +0500

    add page loader, add not found page, add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { ErrorBoundary } from 'react-error-boundary';
+import { PageError } from 'widgets/PageError';
+import { Suspense } from 'react';
 import App from './app/App';
 
 import 'shared/config/i18n/i18n';
@@ -8,8 +11,15 @@ import 'shared/config/i18n/i18n';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary fallback={(
+      <Suspense fallback="">
+        <PageError />
+      </Suspense>
+    )}
+    >
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </BrowserRouter>,
 );
